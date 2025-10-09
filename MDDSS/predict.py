1f293f85592007798a6dc8f667179ee50b18f3a5
xys0416 <77679468+xys0416@users.noreply.github.com>
2025-10-09 09:42:14 +0800

    Update predict.py

--- MDDSS/predict.py
+++ MDDSS/predict.py
@@ -5,37 +5,57 @@ import numpy as np
 from DeepLabV3APSS import DeepLabV3ASPP
 from ReadDataSet import ReadDataset
 
+# Color map for each segmentation class
 label_color = {"head": [255, 0, 255], "shadow": [0, 255, 0]}
-Predict_Read_Path = ".//Hellas_True//"
-Predict_Save_Path = ".//PredictMask_Hellas//"
+
+# Paths
+Predict_Read_Path = ".//MDDD_Cascade_output_true//"   # Folder containing input images
+Predict_Save_Path = ".//PredictMask//"                # Folder to save predicted masks
 weights_path = ".//new_beight//DeepLabV3ASPP35000_96.472.ckpt"
 
+# Build and load model
 model = DeepLabV3ASPP()
 model.load_weights(weights_path)
-for i, j, k in os.walk(Predict_Read_Path):  # 获取文件夹中的所有文件名称
-    catalogList = k  # k是个列表，包含所有该目录下的文件名
-catalogList = catalogList
-i = 0
-len_list = len(catalogList)
-for file_name in catalogList:
-    image = cv2.imread(Predict_Read_Path + file_name)
-    org_h, org_w, org_c = image.shape
+
+# Collect all file names in the read directory
+catalogList = []
+for dirpath, dirnames, filenames in os.walk(Predict_Read_Path):
+    catalogList = filenames  # 'filenames' is a list of all files in the directory
+    break  # Only need the top-level directory
+
+total_files = len(catalogList)
+
+# Process each image
+for idx, file_name in enumerate(catalogList):
+    # Read and resize image
+    image = cv2.imread(os.path.join(Predict_Read_Path, file_name))
+    if image is None:
+        print(f"Warning: Unable to read image {file_name}, skipping.")
+        continue
+    org_h, org_w, _ = image.shape
     image = cv2.resize(image, (256, 256))
 
-    x = tf.convert_to_tensor([image], dtype=tf.float32) / 255
+    # Prepare input tensor
+    x = tf.convert_to_tensor([image], dtype=tf.float32) / 255.0
+
+    # Predict segmentation mask
     out = model(x)
+    out = tf.argmax(out, axis=-1).numpy()  # Shape: (1, 256, 256)
 
-    out = tf.argmax(out, axis=-1).numpy()
-    show_boart_background = np.zeros((out.shape[0], out.shape[1], out.shape[2], 3), dtype=np.uint8)
-    for ind, type_object in enumerate(label_color.keys()):
-        show_boart_background[out == ind + 1] = label_color[type_object]
+    # Create colored mask
+    show_board_background = np.zeros((out.shape[1], out.shape[2], 3), dtype=np.uint8)
+    for class_idx, class_name in enumerate(label_color.keys()):
+        # class_idx+1 because 0 is background
+        show_board_background[out[0] == (class_idx + 1)] = label_color[class_name]
 
-    show_boart_background = np.squeeze(show_boart_background)
-    show_boart_background = cv2.resize(show_boart_background, (org_w, org_h))
-    cv2.imwrite(Predict_Save_Path + file_name, show_boart_background)
+    # Resize back to original size and save
+    show_board_background = cv2.resize(show_board_background, (org_w, org_h))
+    cv2.imwrite(os.path.join(Predict_Save_Path, file_name), show_board_background)
 
-    print("\r", end="")
-    print("Predict progress %d/%d : " % (i, len_list), "▮" * int((50 * i / len_list)),
-          "▯" * int(50 - (50 * i / len_list)), "  %.2f %%" % (100 * i / len_list), end="")
-    i += 1
+    # Progress bar
+    progress = int(50 * idx / total_files)
+    print("\rPredict progress %d/%d : " % (idx + 1, total_files),
+          "▮" * progress, "▯" * (50 - progress),
+          "  %.2f %%" % (100 * (idx + 1) / total_files), end="")
 
+print("\nPrediction finished.")
